9581cf7112d829de7b10ebbfeef898d070a3efac
natuvide <n-videmato@hotmail.com>
2023-05-28 02:23:18 -0300

    class rotar para animacion

--- js/main.js
+++ js/main.js
@@ -3,26 +3,26 @@ let nav_bar = `   <nav>
 <div class="contenedor">
 
 <a href="index.html">
-<img class="logo1" src="./img/logopizzadays.png" alt=""> 
+<img class="logo1 rotar" src="./img/logopizzadays.png" alt=""> 
 </a>   
 </div>
 
 
 <ul>
 
-    <li><a href="pizzasdatos.html">Carta</a></li>
-    <li><a href="2-reservas.html">Reservas</a></li>
-    <li><a href="3-ubicacion.html">Ubicación</a></li>
+    <li class="opcion_menu"><a href="pizzasdatos.html">Carta</a></li>
+    <li class="opcion_menu"><a href="2-reservas.html">Reservas</a></li>
+    <li class="opcion_menu"><a href="3-ubicacion.html">Ubicación</a></li>
 </ul>
 
 </nav>
 `
 let footer= ` <footer>
 <p class="pfooter">Podes seguirnos en nuestras redes!</p>
-<ul>
+<ul class= "redes">
 
     <li class="icon_margin">
-        <a href="https://www.facebook.com/pizzadays1" target="_blank"><i class="fa-brands fa-facebook fa-3x"
+        <a  href="https://www.facebook.com/pizzadays1" target="_blank"><i class="fa-brands fa-facebook fa-3x"
                 style="color: #ffffff;"></i>
 
         </a>
